8f9a81d1c80a29d286d5562b84aee889755d0d8b
JardsonVenancio <126803792+jardson123442@users.noreply.github.com>
2024-05-08 17:28:35 -0300

    Using DTO pattern to return users

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,26 +1,29 @@
 package com.example.demo.resources;
 
 import com.example.demo.domain.User;
+import com.example.demo.dto.UserDTO;
 import com.example.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 
     @Autowired
     private UserService service;
 
-    @RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
