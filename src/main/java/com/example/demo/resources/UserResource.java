378a21ec0111f83b2b2ed1ed2a7d51538cd8fd5e
JardsonVenancio <126803792+jardson123442@users.noreply.github.com>
2024-05-08 17:10:09 -0300

    Conect  MongoDB to repository and service

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,25 +1,26 @@
 package com.example.demo.resources;
 
 import com.example.demo.domain.User;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value="/users")
 public class UserResource {
 
-    @GetMapping
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(method= RequestMethod.GET)
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "maria", "maria@gmail.com");
-        User alex = new User("2", "alex", "alex@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
